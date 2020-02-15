263daf1a8ff1bba382fa1b893472796ba910312f
Tammana Sultana <44374697+TamSultana203@users.noreply.github.com>
2020-02-16 00:33:33 +0600

    Add files via upload

---
+++ ClassInstance.py
@@ -0,0 +1,20 @@
+class Employee:
+    def  __init__(self, first, last, pay):
+        self.first =first
+        self.last = last
+        self.pay = pay
+        self.email = first+'.'+last+'@gmail.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+emp_1 = Employee('Correy', 'Strat', 1000)
+emp_2 = Employee('shanon','stres', 999)
+
+
+
+print(emp_1.email)
+print(emp_2.email)
+
+print(emp_1.fullname())
